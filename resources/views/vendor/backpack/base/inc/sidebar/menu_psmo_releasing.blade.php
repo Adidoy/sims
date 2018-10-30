419fd147a8816f7a9fc7b8050c2a31fa242c7c24
Adi Ablir <abliradrian@gmail.com>
2018-10-30 20:03:17 +0800

    Creation of menu_psmo_releasing

--- resources/views/vendor/backpack/base/inc/sidebar/menu_psmo_releasing.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar/menu_psmo_releasing.blade.php
@@ -4,6 +4,6 @@
         <a href="#"><i class="fa fa-share" aria-hidden="true"></i><span>Requesition and Issuance</span><i class="fa fa-angle-left pull-right"></i></a>
         <ul class="treeview-menu">
             <li><a href="{{ url('request') }}"><li><i class="fa fa-list" aria-hidden="true"></i>View</a></li>
-            <li><a href="@if(Auth::user()->access == 1) {{ url('inventory/supply/stockcard/release') }} @endif"><li><i class="fa fa-pencil" aria-hidden="true"></i>Allocate</a></li>
+            <li><a href="{{ url('inventory/supply/stockcard/release') }}"><li><i class="fa fa-pencil" aria-hidden="true"></i>Allocate</a></li>
         </ul>
     </li>
